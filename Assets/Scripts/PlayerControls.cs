2c206fc6ffba49e76e4829cec3130e180ddc9a78
Aditya Kumar Nayak <adityak23100@iiitnr.edu.in>
2024-12-17 01:19:09 +0530

    movementworking!

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
@@ -8,9 +11,15 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] float vertical;
     [SerializeField] bool attackpressed;
 
+    [SerializeField] float acceleration;
+    [SerializeField] float deacceleration;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float velPower;
+
+    Rigidbody2D rb;
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -18,5 +27,13 @@ public class PlayerControls : MonoBehaviour
         attackpressed = Input.GetKey(KeyCode.Mouse0);
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate(){
+        float targetSpeed = horizontal*moveSpeed;
+        float speedDiff = targetSpeed - rb.velocity.x;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration:deacceleration;
+        float force = Mathf.Pow(Mathf.Abs(speedDiff)*accelRate, velPower) * Mathf.Sign(speedDiff);
+
+        rb.AddForce(force * Vector2.right);
     }
 }
