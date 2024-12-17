5400f41a52b70a3401aba7a88bb62dd74d3ca869
Aditya Kumar Nayak <adityak23100@iiitnr.edu.in>
2024-12-17 16:41:47 +0530

    working

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -10,11 +10,13 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] float horizontal;
     [SerializeField] float vertical;
     [SerializeField] bool attackpressed;
+    [SerializeField] bool jumpPressed;
 
     [SerializeField] float acceleration;
     [SerializeField] float deacceleration;
     [SerializeField] float moveSpeed;
     [SerializeField] float velPower;
+    [SerializeField] float jumpForce;
 
     Rigidbody2D rb;
     void Start()
@@ -25,10 +27,24 @@ public class PlayerControls : MonoBehaviour
     void Update()
     {
         attackpressed = Input.GetKey(KeyCode.Mouse0);
+        jumpPressed = Input.GetKey(KeyCode.Space);
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        if(Input.GetKeyDown(KeyCode.Space)){
+            jump();
+        }
     }
+
     void FixedUpdate(){
+        movement();
+        
+    }
+
+
+    void jump(){
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+    void movement(){
         float targetSpeed = horizontal*moveSpeed;
         float speedDiff = targetSpeed - rb.velocity.x;
         float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration:deacceleration;
