95308cc38b8f2b7a9823e2e325ecd9837706fc4a
Aditya Kumar Nayak <adityak23100@iiitnr.edu.in>
2025-01-29 01:13:14 +0530

    slowly building up!

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField]private Transform cameraTransform;
+    
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
